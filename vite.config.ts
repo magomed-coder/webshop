7d1a6a4860571498b1dd996af4b64a4f6653a10f
Magomed Khadzhiev <m.hadjiev@dhabits.ru>
2026-01-10 19:20:42 +0300

    - fix для хостинга

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), , tailwindcss(), tsconfigPaths()],
+  base: "/",
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
       "@components": path.resolve(__dirname, "src/components"),
